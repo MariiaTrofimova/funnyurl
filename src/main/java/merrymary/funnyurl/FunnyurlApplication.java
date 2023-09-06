3279784aed89e7c662968c4ae64c8fa8ced8baa4
MariiaTrofimova <mariiatr@yandex.ru>
2023-09-06 14:21:12 +0300

    feat: создан проект с Gradle и Spring Boot

---
+++ src/main/java/merrymary/funnyurl/FunnyurlApplication.java
@@ -0,0 +1,13 @@
+package merrymary.funnyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyurlApplication.class, args);
+	}
+
+}
